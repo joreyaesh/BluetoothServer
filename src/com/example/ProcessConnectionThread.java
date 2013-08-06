bfb65a566965bb2db3dfff8951ee0a443f9ff361
Joreyaesh <joreyaesh@github.com>
2013-08-06 00:58:25 -0400

    Worked on sharing files

--- src/com/example/ProcessConnectionThread.java
+++ src/com/example/ProcessConnectionThread.java
@@ -11,9 +11,12 @@ import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.io.OutputStream;
+import java.io.StreamCorruptedException;
 
 import javax.microedition.io.StreamConnection;
 
@@ -22,15 +25,17 @@ public class ProcessConnectionThread implements Runnable{
 	private StreamConnection mConnection;
 	
 	// Constants that indicate command from devices
-	private static final int CHECK_CONNECTION = -1;
-	private static final int TYPE = 0;
-	private static final int MOUSE_LEFT = 1;
-	private static final int MOUSE_RIGHT = 2;
-	private static final int MOUSE_SCROLL = 5;
-	private static final int MOVE_MOUSE_BY = 10;
-	private static final int BACKSPACE = 25;
-	private static final int ENTER = 20;
-	private static final int NEW_TAB = 30;
+	public static final int CHECK_CONNECTION = -1;
+	public static final int MOUSE_LEFT = 1;
+	public static final int MOUSE_RIGHT = 2;
+	public static final int MOUSE_SCROLL = 5;
+	public static final int MOVE_MOUSE_BY = 10;
+	public static final int TYPE = 15;	
+	public static final int BACKSPACE = 25;
+	public static final int ENTER = 20;
+	public static final int NEW_TAB = 30;
+	public static final int SHARE_IMAGE = 50;
+	public static final int SHARE_IMAGE_FILE_SIZE = 51;
 	
 	// Robot to control the computer
 	private Robot robot = null;
@@ -42,13 +47,28 @@ public class ProcessConnectionThread implements Runnable{
 	static int x = 0;
 	static int y = 0;
 	
+	// Parameter to hold file size of incoming file
+	static int fileSize = 0;
+	
 	// Boolean to keep track of whether or not the bluetooth connection is active
 	public static boolean isActive = false;
+	
+	// InputStream
+	InputStream inputStream = null;
 	
 	public ProcessConnectionThread(Rectangle screen, StreamConnection connection)
 	{
 		rectScreen = screen;
 		mConnection = connection;
+	}
+	
+	private void connectionLost(){
+		System.out.println("Connection lost...");
+		//robot.keyPress(KeyEvent.VK_ALT);
+		//robot.keyPress(KeyEvent.VK_F4);
+		//robot.keyRelease(KeyEvent.VK_F4);
+		//robot.keyRelease(KeyEvent.VK_ALT);
+		isActive = false;
 	}
 	
 	/**
@@ -63,24 +83,30 @@ public class ProcessConnectionThread implements Runnable{
 		}
 		try {
 			// prepare to receive data
-			InputStream inputStream = mConnection.openDataInputStream();
+			inputStream = mConnection.openDataInputStream();
 			System.out.println("Connected!");
 			isActive = true;
-			byte[] data = new byte[1024];
 			while (isActive) {
-				Arrays.fill(data, (byte) 0);
+				byte[] data = new byte[1024];
 				try{
 					inputStream.read(data);
 				}catch(NullPointerException e){
-					e.printStackTrace();
+					e.printStackTrace();	
 				}catch(IOException e2){
 					e2.printStackTrace();
 				}
-				RemoteCommand rc = RemoteCommand.getRemoteCommand(data);
-				processCommand(rc);
+				try{
+					RemoteCommand rc = RemoteCommand.getRemoteCommand(data);
+					processCommand(rc, data);
+				} catch (StreamCorruptedException e){
+					// Handle an incoming image
+					processCommand(new RemoteCommand(SHARE_IMAGE), data);
+				}
+				
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.err.println("Error downloading object.");
 		} 
 	}
 	
@@ -88,7 +114,7 @@ public class ProcessConnectionThread implements Runnable{
 	 * Process the command from client
 	 * @param command the command code
 	 */
-	private void processCommand(RemoteCommand rc) {
+	private void processCommand(RemoteCommand rc, byte[] data) {
 		try {
 			switch (rc.command) {
 			case MOUSE_RIGHT:
@@ -126,19 +152,67 @@ public class ProcessConnectionThread implements Runnable{
 			case CHECK_CONNECTION:
 				// Do nothing
 				break;
+			case SHARE_IMAGE_FILE_SIZE:
+				fileSize = rc.parameter1;
+				break;
+			case SHARE_IMAGE:
+				handlePicture(fileSize);
+				break;
 
 			default:
 				break;
 			}
 		} catch(NullPointerException e) {
-			System.out.println("Connection lost...");
-			robot.keyPress(KeyEvent.VK_ALT);
-			robot.keyPress(KeyEvent.VK_F4);
-			robot.keyRelease(KeyEvent.VK_F4);
-			robot.keyRelease(KeyEvent.VK_ALT);
-			isActive = false;
+			connectionLost();
 		}
 	}
+	
+//	private void handlePicture(byte[] data) {
+//		try {
+//			File file = new File("tmp.jpg");
+//			if(!file.delete())
+//				file.createNewFile();
+//			FileOutputStream fos = new FileOutputStream(file);
+//			fos.flush();
+//			fos.write(data, 0, 3101);
+//			
+//			fos.flush();
+//			fos.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		// TODO display the file
+//	}
+	
+	private void handlePicture(int fileSize) {
+		try {
+			File file = new File("tmp.jpg");
+        	if(!file.delete())
+				file.createNewFile();
+            OutputStream out = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0, counter = 0;
+ 
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+            	out.write(buffer, 0, bytesRead);
+            	counter += bytesRead;
+            	System.out.println("total bytes read: " + counter);
+            	if(counter >= fileSize){
+            		break;
+            	}
+            }
+ 
+            System.out.println("Download Successfully!");
+            System.out.println("Filesize: " + fileSize);
+            fileSize = 0;
+            out.flush();
+            out.close();
+ 
+        } catch (Exception e) {
+        	e.printStackTrace();
+            System.out.println("Error on downloading file!");
+        }
+    }
 
 	private void newTab(){
 		// simulates a CTRL+T event
--- src/com/example/RemoteCommand.java
+++ src/com/example/RemoteCommand.java
@@ -3,7 +3,6 @@ package com.example;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
@@ -14,46 +13,48 @@ public class RemoteCommand implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 5172852401L;
+	private static final long serialVersionUID = 1111L;
 	/**
 	 * 
 	 */
 	public int command = 0;
-	public int parameter1 = 0; // X coordinate change/scroll amount
+	public int parameter1 = 0; // X coordinate change/scroll amount/file size
 	public int parameter2 = 0; // Y coordinate change
 	public String string1 = ""; // String for textbox
 
 	public RemoteCommand() {
 	}
+	
+	public RemoteCommand(int command){
+		this.command = command;
+	}
 
-	public static RemoteCommand getRemoteCommand(byte data[]) {
+	public static RemoteCommand getRemoteCommand(byte data[]) throws StreamCorruptedException {
 		ByteArrayInputStream bis = new ByteArrayInputStream(data);
-		//System.err.println("received " + data.length + " bytes: " + data[0] + "," + data[1] + "," + data[2] + "," + data[3]);
 		ObjectInputStream in = null;
 		try {
 			in = new ObjectInputStream(bis);
 			RemoteCommand ret = (RemoteCommand) in.readObject();
 			return ret;
-		} catch (StreamCorruptedException e) {
-			// Do nothing... Connection lost
-		} catch (InvalidClassException e) {
-			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			// read the stream header to check if connection is lost, or image was received
+			for(int i = 0; i < 8; i++){
+				if(bis.read() != 0){ // if any of the header bytes != 0, an image was received
+					throw new StreamCorruptedException();
+				}
+			}
+		} 
+		catch (ClassNotFoundException e) {}
+		try {
+			bis.close();
+			if(in != null)in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				bis.close();
-				if(in != null)in.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-				
-			}
-		}
+		} 
 		return null;
-
 	}
+
 
 	public byte[] getByteArray() {
 		byte[] retval = null;
@@ -64,13 +65,13 @@ public class RemoteCommand implements Serializable {
 			out.writeObject(this);
 			return bos.toByteArray();
 		} catch (IOException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 		} finally {
 			try {
 				out.close();
 				bos.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				//e.printStackTrace();
 			}
 		}
 		return retval;
